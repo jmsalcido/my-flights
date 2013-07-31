4791869a2e884961bc96bb615b3ebb749d69de2a
Jose Miguel Salcido Aguilar <jsalcido@nearsoft.com>
2013-07-31 09:51:21 -0700

    [A] CORS filter added, Airports Controller using it for now.

--- project/src/com/nearsoft/myflights/controllers/AirportController.java
+++ project/src/com/nearsoft/myflights/controllers/AirportController.java
@@ -1,9 +1,17 @@
 package com.nearsoft.myflights.controllers;
 
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,11 +19,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.gson.Gson;
 import com.nearsoft.myflights.service.AirportService;
 
 @Controller
 @RequestMapping("/")
 public class AirportController {
+
+    private final Log logger = LogFactory.getLog(getClass());
 
     @Autowired
     AirportService airportService;
@@ -25,15 +36,32 @@ public class AirportController {
 
     @RequestMapping(value = { "airports/{word}" }, method = { RequestMethod.GET })
     @ResponseBody
-    public Map<String, Object> listAirportsByKeyword(
-            @PathVariable String word) {
-        Map<String, Object> map = new HashMap<String, Object>(); 
-        if(StringUtils.isEmpty(word)) {
-            map.put("error", "'word' can't be empty");
+    public ResponseEntity<String> listAirportsByKeyword(@PathVariable String word,
+            HttpServletRequest request) {
+        logger.info(String.format("Incoming request from %s :)",request.getLocalAddr()));
+        Map<String, Object> responseMap = new HashMap<String, Object>();
+        if (StringUtils.isEmpty(word)) {
+            responseMap.put("error", "'word' can't be empty");
         } else {
-            map.put("airports", airportService.getAirportsByKeyword(word));
+            responseMap.put("airports", airportService.getAirportsByKeyword(word));
         }
-        return map;
+
+        // this could be on an util class for every controller that returns JSON.
+        HttpHeaders responseHeaders = new HttpHeaders();
+
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String nextElement = (String) headerNames.nextElement();
+            //System.out.println(nextElement + "=" + request.getHeaders(nextElement).toString()); 
+            responseHeaders.set(nextElement, request.getHeader(nextElement));
+        }
+
+        // populating the header required for CORS
+        responseHeaders.set("Access-Control-Allow-Origin", "http://localhost:3000");
+
+        Gson gson = new Gson();
+        
+        return new ResponseEntity<String>(gson.toJson(responseMap), responseHeaders, HttpStatus.OK);
     }
 
 }
