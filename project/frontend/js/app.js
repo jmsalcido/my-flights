6ba000cae1e14ee625eadd13cd77f093100e9075
Jose Miguel Salcido Aguilar <jsalcido@nearsoft.com>
2013-08-05 19:20:28 -0700

    updating the frontend with a new approach for autocomplete, will work on this tonight: MONDAY/AUG/2013

--- project/frontend/js/app.js
+++ project/frontend/js/app.js
@@ -1,72 +1,131 @@
 App = Ember.Application.create();
 
-JQ.AutoComplete = Em.TextField.extend(JQ.Widget, {
-  uiType: 'autocomplete',
-  uiOptions: ['source', 'autoFocus', 'delay', 'disabled', 'minLength', 'position']
-});
-
-JQ.ButtonView = Em.View.extend(JQ.Widget, {
-  uiType: 'button',
-  uiOptions: ['label', 'disabled'],
-  tagName: 'button'
-});
-
-App.ButtonView = JQ.ButtonView.extend({
-  click: function() {
-    this.set('disabled', true);
-    this.get('controller').consoleIt('huehuehue');
-  }
+// extend ember textfield to accept autocomplete and other attributes
+App.TextField = Ember.TextField.extend({
+    attributeBindings: ['accept', 'autocomplete', 'autofocus', 'name', 'required']
 });
 
 App.RoutesView = Ember.View.extend();
 
 App.RoutesController = Ember.Controller.extend({
-  airports: null,
-  departureText: null,
-  arrivalText: null,
-  testText: "",
-  autocomplete: function(request, response) {
-    response([request.term]);
-  },
-  searchResults: function(){
-    var searchText = this.get('departureText');
-    if(!searchText) { return; }
-    var search = App.Search.find(searchText);
-    var airports = search.get('airports');
-    return airports;
-  }.property('departureText'),
-  consoleIt: function(param) {
-    console.log(param);
-  }
-})
-
-App.AutoComplete = JQ.AutoComplete.extend({
-    source: function (e) {
-        var query = $('#tags').val(),
-            resultList = $('#results'),
-            print = function (res_obj){
-                var array = res_obj.airports,
-                    length = res_obj.airports.length;
-
-                for(var i = 0; i < length; i += 1){
-                    var country = array[i].name;
-                    console.log(array[i]);
-                    //TODO: replace this approach... Better if you clone the node, append all that shit and then, replace the original...
-                    resultList.append($('<li></li>').text(country));
-                }
-                $('#tags').autocomplete({
-            source: array
-        });
-            };
-        resultList.html('');
-        jQuery.App.SearchFn(query, print);
+    airports: null,
+    departureText: null,
+    arrivalText: null,
+    title: 'Select your route',
+    searchResults: function(){
+        var searchText = this.get('departureText') || this.get('arrivalText');
+        if(!searchText) { return; }
+        // server request
+        var search = App.Search.find(searchText);
+        var airports = search.get('airports');
+        return airports;
+    }.property('departureText', 'arrivalText'),
+    select: function(airport) {
+        this.set('departureText', airport.get('code'));
+        this.get('departureText').enabled = 'false';
     }
 });
+
+App.AutocompleteView = Ember.View.extend({
+    isInvisible: function() {
+        var searchResults = this.get('searchResults');
+        return searchResults !== null ? "invisible" : "visible";
+    }
+});
+
+// PLEASE DELETE ME
+// PLEASE DELETE ME
+// PLEASE DELETE ME
+App.Person = Ember.Object.extend({
+    id: 0,
+    first_name: null,
+    last_name: null,
+    fullName: function() {
+        return this.get('first_name') + " " + this.get('last_name');
+    }.property('first_name', 'last_name')
+});
+
+App.TestController = Ember.Controller.extend({
+    selected: null,
+    select: null,
+    checkBox_disabled: true,
+    disableCheckBox: function() {
+        console.log(checkBox_disabled);
+    }.observes('checkBox_disabled'),
+    people: [],
+    tmpFirstName: null,
+    tmpLastName: null,
+    // check box
+    createPerson: false,
+    _createPerson: function() {
+        var createPerson = this.get('createPerson');
+        console.log('createPerson changed to %@'.fmt(createPerson));
+    }.observes('createPerson'),
+    moveAlong: function() {
+        var first_name = this.get('selected').get('first_name');
+        var last_name = this.get('selected').get('last_name');
+        this.set('tmpFirstName', first_name);
+        this.set('tmpLastName', last_name);
+    }.observes('selected'),
+    usePerson: function(createPerson) {
+        if(createPerson) {
+            var person = App.Person.create();
+            person.set('first_name', this.get('tmpFirstName'));
+            person.set('last_name', this.get('tmpLastName'));
+            person.addObserver('fullName', function() {
+                console.log("%@: Y U CHANGE MUH NAME?".fmt(this.get('fullName')));
+            });
+            // PUSHOBJECT() here.
+            this.get('people').pushObject(person);
+            console.log(person.get('fullName') + " says: hello!");
+
+            // reset inputs.
+            this.set('tmpFirstName', null);
+            this.set('tmpLastName', null);
+            this.set('createPerson', false);
+        } else {
+            console.log(this.get('checkBox'));
+            console.log('fuck you')
+        }
+    }
+});
+
+App.OTFusionLinkView = Ember.View.extend({
+    tagName: "a",
+    attributeBindings: ['href'],
+    href: "http://otfusion.org"
+});
+
+App.ApplicationController = Ember.Controller.extend({
+});
+
+// App.AutoComplete = JQ.AutoComplete.extend({
+//     source: function (e) {
+//         var query = $('#tags').val(),
+//             resultList = $('#results'),
+//             print = function (res_obj){
+//                 var array = res_obj.airports,
+//                     length = res_obj.airports.length;
+//
+//                 for(var i = 0; i < length; i += 1){
+//                     var country = array[i].name;
+//                     console.log(array[i]);
+//                     //TODO: replace this approach... Better if you clone the node, append all that shit and then, replace the original...
+//                     resultList.append($('<li></li>').text(country));
+//                 }
+//                 $('#tags').autocomplete({
+//             source: array
+//         });
+//             };
+//         resultList.html('');
+//         jQuery.App.SearchFn(query, print);
+//     }
+// });
 
 App.RESTSerializer = DS.RESTSerializer.extend({
   init: function() {
     this._super();
-  }
+}
 });
 
 App.Search = DS.Model.extend({
@@ -97,25 +156,25 @@ App.Store = DS.Store.extend({
     mappings: { 
       airports: App.Airport,
       search: App.Search
-    },
-    url: 'http://localhost:8080',
-    namespace: 'MyFlights',
-    serializer: App.RESTSerializer
-  })
+  },
+  url: 'http://localhost:8080',
+  namespace: 'MyFlights',
+  serializer: App.RESTSerializer
+})
 });
 
-$(function($){
+// $(function($){
 
-    $.App = $.App || {};
+//     $.App = $.App || {};
 
-    $.App.SearchFn = function (query, fn) {
-        $.ajax({
-                url: 'http://localhost:8080/MyFlights/search/' + query
-            })
-            .done(function (e){
-                fn.call(this, JSON.parse(e));
-            })
-            .error(function (e){});
-    };
+//     $.App.SearchFn = function (query, fn) {
+//         $.ajax({
+//                 url: 'http://localhost:8080/MyFlights/search/' + query
+//             })
+//             .done(function (e){
+//                 fn.call(this, JSON.parse(e));
+//             })
+//             .error(function (e){});
+//     };
 
-}(jQuery));
+// }(jQuery));
