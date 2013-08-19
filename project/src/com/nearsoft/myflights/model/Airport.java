eb14ebcb897379777f2d6f7c54d169acc90399f3
Jose Miguel Salcido Aguilar <jsalcido@nearsoft.com>
2013-08-19 12:54:45 -0700

    removing dead code

--- project/src/com/nearsoft/myflights/model/Airport.java
+++ project/src/com/nearsoft/myflights/model/Airport.java
@@ -1,30 +1,14 @@
 package com.nearsoft.myflights.model;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Airport {
 
     public final static String FS_CODE = "fs";
-    public final static String IATA_CODE = "iata";
-    public final static String ICAO_CODE = "icao";
-    public final static String FAA_CODE = "faa";
 
     private int id;
-    //private Map<String, String> codes;
     private String code;
     private String name;
     private String city;
     private String country;
-
-    public static Airport createAirportFromFSCode(String fsCode) {
-        Airport airport = new Airport();
-        Map<String, String> map = new HashMap<String, String>();
-        map.put(Airport.FS_CODE, fsCode);
-        airport.setCode(fsCode);
-        //airport.setCodes(map);
-        return airport;
-    }
 
     public int getId() {
         return id;
