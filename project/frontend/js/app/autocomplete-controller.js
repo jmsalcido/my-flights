e74359af700cb41a65996b09d0b2d9eb961eb266
Jose Miguel Salcido Aguilar <jsalcido@nearsoft.com>
2013-08-19 12:45:34 -0700

    Deleting files not used

--- project/frontend/js/app/autocomplete-controller.js
+++
@@ -1,9 +0,0 @@
-App.AutocompleteController = Ember.Controller.extend({
-  search: function(term, context) {
-    var results = [
-      Ember.Object.create({name: 'Bison'}),
-      Ember.Object.create({name: 'Vega'}),
-    ];
-    context.set('content', results);
-  }
-});
