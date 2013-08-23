5ea8b4386591eeeb482dae254ee15422822d05a2
Jose Miguel Salcido Aguilar <jsalcido@nearsoft.com>
2013-08-22 20:23:30 -0700

    Adding ReservationController and Reservation model at the backend, Controller needs more CORS filters (researching), maybe will merge /frontend and /WebContent

---
+++ project/src/com/nearsoft/myflights/controllers/ReservationController.java
@@ -0,0 +1,51 @@
+package com.nearsoft.myflights.controllers;
+
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+import com.nearsoft.myflights.model.Reservation;
+
+@Controller
+@RequestMapping("/")
+public class ReservationController {
+    
+    private final Log logger = LogFactory.getLog(getClass());
+    
+    @RequestMapping(headers ={"Accept=application/json"},
+                    method = { RequestMethod.POST },
+                    value = "reservations")
+    @ResponseBody
+    public ResponseEntity<String> createReservation(@RequestBody final Reservation reservation, HttpServletRequest request, HttpServletResponse response) {
+        logger.info(reservation);
+     // this could be on an util class for every controller that returns
+        // JSON.
+        HttpHeaders responseHeaders = new HttpHeaders();
+
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String nextElement = (String) headerNames.nextElement();
+            responseHeaders.set(nextElement, request.getHeader(nextElement));
+        }
+
+        // populating the header required for CORS
+        responseHeaders.set("Access-Control-Allow-Origin", "*");
+        Gson gson = new Gson();
+        return new ResponseEntity<String>(gson.toJson(reservation), responseHeaders,
+                HttpStatus.OK);
+    }
+
+}
