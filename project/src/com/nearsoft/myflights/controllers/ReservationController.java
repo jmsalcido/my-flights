c183f921b2af8aea7774875b509a05c82c3fc9ad
Jose Miguel Salcido Aguilar <jsalcido@nearsoft.com>
2013-08-23 21:31:53 -0700

    removing classes that should not be added

--- project/src/com/nearsoft/myflights/controllers/ReservationController.java
+++ project/src/com/nearsoft/myflights/controllers/ReservationController.java
@@ -21,29 +21,5 @@ import com.nearsoft.myflights.model.Reservation;
 public class ReservationController {
     
     private final Log logger = LogFactory.getLog(getClass());
-    
-    @RequestMapping(headers ={"Accept=application/json"},
-                    method = { RequestMethod.POST },
-                    value = "reservations")
-    public @ResponseBody Reservation createReservation(@RequestBody String reservation, HttpServletRequest request, HttpServletResponse response) {
-        logger.info(request.getServletPath());
-        // this could be on an util class for every controller that returns
-        // JSON.
-        HttpHeaders responseHeaders = new HttpHeaders();
-//
-//        Enumeration<String> headerNames = request.getHeaderNames();
-//        while (headerNames.hasMoreElements()) {
-//            String nextElement = (String) headerNames.nextElement();
-//            responseHeaders.set(nextElement, request.getHeader(nextElement));
-//        }
-//
-//        // populating the header required for CORS
-//        responseHeaders.set("Access-Control-Allow-Origin", "*");
-//        Gson gson = new Gson();
-//        return new ResponseEntity<String>(gson.toJson(new Object()), responseHeaders,
-//                HttpStatus.OK);
-
-        return new Reservation();
-    }
 
 }
