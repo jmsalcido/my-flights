5c2f71458a849f1059e3ed4529945a85e791228b
Jose Miguel Salcido Aguilar <jose@otfusion.org>
2013-08-25 11:55:06 -0700

    Adding the new branch and removing the old dir structure

--- project/src/com/nearsoft/myflights/service/FlightService.java
+++
@@ -1,9 +0,0 @@
-package com.nearsoft.myflights.service;
-
-import java.util.Map;
-
-public interface FlightService {
-
-    public Map<String, Object> getFlights(String from, String to, String date) throws Exception;
-
-}
